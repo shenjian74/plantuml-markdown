ec01e1862e91cdc1cdf29851e0f2e1e2f45a2215
Shen Jian <shenjian74@gmail.com>
2015-06-17 23:24:45 +0800

    Modified   README.md Modified   preprocess.py
    Signed-off-by: Shen Jian <shenjian74@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # plantuml-markdown
-parse plantuml tags in markdown
+parse plantuml tags in markdown (NOT yet complete!)
+
+For Example:
+
+Markdown:
+
+    # Title1
+	## Title2
+	
+	content
+	
+	```uml
+	@startuml
+	Alice -> Bob: Authentication Request
+	Bob --> Alice: Authentication Response
+
+	Alice -> Bob: Another authentication Request
+	Alice <-- Bob: another authentication Response
+	@enduml
+	```
+
+Convert to:
+
+    # Title1
+	## Title2
+
+	content
+	
+```uml
+@startuml
+Alice -> Bob: Authentication Request
+Bob --> Alice: Authentication Response
+
+Alice -> Bob: Another authentication Request
+Alice <-- Bob: another authentication Response
+@enduml
+```
+	
+1. Download and Install [plantuml](http://www.plantuml.com/)
+2. Config the path of [plantuml.jar](http://sourceforge.net/projects/plantuml/files/plantuml.jar/download) in "preprocess.py"
+3. Finally, "python preprocess.py"
+
--- preprocess.py
+++ preprocess.py
@@ -6,12 +6,23 @@ import re
 import logging
 import tempfile
 import argparse
+from platform import popen
 
-pattern = re.compile("```uml.*```", re.DOTALL)
+pattern = re.compile("```uml(?P<content>.*)```", re.DOTALL)
 os_tempdir = tempfile.gettempdir()
 logging.basicConfig(format='[%(filename)s:%(lineno)d] : %(asctime)s : %(levelname)s : %(message)s',
                     level=logging.INFO)
 
+
+def convert2png(content, filename):
+    fp, tmpfilename = tempfile.mkstemp()
+    os.write(fp, content)
+    os.close(fp)
+    cmdline = 'java -jar plantuml.jar -tpng %s' % tmpfilename
+    print 'cmd:%s' % cmdline
+    result = os.popen(cmdline, 'r')
+    print result.readlines()
+    os.remove(tmpfilename)
 
 def main(args=None):
 
@@ -29,19 +40,23 @@ def main(args=None):
         if script is None:
             break
         tmpfilename = os.path.join(os_tempdir, 'plantuml_img%d.png' % index)
+        convert2png(script.group('content'), tmpfilename)
         file_content = re.sub(pattern, lambda s: "![](%s)" % tmpfilename, file_content)
         index += 1
 
     fp, tmpfilename = tempfile.mkstemp()
     os.write(fp, file_content)
     os.close(fp)
-    print tmpfilename
-
-    # cmdline = 'java -jar plantuml.jar -tpng'
-    # result = os.popen(cmdline, 'r')
-    # for line in result.readlines():
-    #     if find(line, "Cross") != -1:
-    #         return atof(split(line)[-1])
+    # cmdline = 'pandoc -S --from=markdown+pipe_tables+yaml_metadata_block ' \
+    #           '--reference-docx "%bat_path%zte_reference.docx" ' \
+    #           '--css="D:\git@OSC\Workspace\projects\CSS\github.css" ' \
+    #           '--table-of-contents -s %p1% -o "%%~df%%~pf%%~nf.docx'
+    cmdline = 'pandoc -S --from=markdown+pipe_tables+yaml_metadata_block --css="github.css" ' \
+              '--table-of-contents -s %s -o output.html' % tmpfilename
+    print cmdline
+    lines = os.popen(cmdline, 'r')
+    print result.readlines()
+    os.remove(tmpfilename)
 
 
 if __name__ == "__main__":
